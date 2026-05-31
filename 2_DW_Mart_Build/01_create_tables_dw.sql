6d93fc797510f73a7de65ac3b61d2d31b2092c7b
Ryan Swanson <swansonryanr@gmail.com>
2026-05-31 12:35:53 -0400

    Create star schema tables for our dw

--- 2_DW_Mart_Build/01_create_tables_dw.sql
+++ 2_DW_Mart_Build/01_create_tables_dw.sql
@@ -1,1 +1,49 @@
 -- Step 1: DW - Create star schema tables
+
+DROP TABLE IF EXISTS skills_job_dim;
+DROP TABLE IF EXISTS job_postings_fact;
+DROP TABLE IF EXISTS company_dim;
+DROP TABLE IF EXISTS skills_dim;
+
+
+CREATE TABLE company_dim (
+    company_id  INTEGER     PRIMARY KEY,
+    name        VARCHAR
+);
+
+CREATE TABLE skills_dim (
+    skill_id   INTEGER     PRIMARY KEY,
+    skill       VARCHAR,
+    type        VARCHAR
+);
+
+CREATE TABLE job_postings_fact (
+    job_id                  INTEGER     PRIMARY KEY,
+    company_id              INTEGER,
+    job_title_short         VARCHAR,
+    job_title               VARCHAR,
+    job_location            VARCHAR,
+    job_via                 VARCHAR,
+    job_schedule_type       VARCHAR,
+    job_work_from_home      BOOLEAN,
+    search_location         VARCHAR,
+    job_posted_date         TIMESTAMP,
+    job_no_degree_mention   BOOLEAN,
+    job_health_insurance    BOOLEAN,
+    job_country             VARCHAR,
+    salary_rate             VARCHAR,
+    salary_year_avg         DOUBLE,
+    salary_hour_avg         DOUBLE,
+    FOREIGN KEY (company_id) REFERENCES company_dim(company_id));
+
+CREATE TABLE skills_job_dim (
+    skill_id    INTEGER,
+    job_id      INTEGER,
+    PRIMARY KEY (skill_id, job_id),
+    FOREIGN KEY (skill_id) REFERENCES skills_dim(skill_id),
+    FOREIGN KEY (job_id) REFERENCES job_postings_fact(job_id) 
+);
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'main';
